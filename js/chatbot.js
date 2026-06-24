d299e349e77f51576c1d790ed1284932e31e66d5
allimpliments <allimpliments@gmail.com>
2026-06-24 13:04:59 +0530

    Add Chatbot object with render method

---
+++ js/chatbot.js
@@ -0,0 +1,10 @@
+const Chatbot = {
+  render() {
+    contentArea.innerHTML = `
+      <div class="card-widget">
+        <h4><i class="fas fa-robot text-purple me-2"></i>Chatbot</h4>
+        <p>Bot replies and flow maker will appear here.</p>
+      </div>
+    `;
+  }
+};
