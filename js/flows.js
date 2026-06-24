43f9a8fa40c6138ba99b925c647f922ab675698f
allimpliments <allimpliments@gmail.com>
2026-06-24 13:04:16 +0530

    Add Flows object with render method

---
+++ js/flows.js
@@ -0,0 +1,10 @@
+const Flows = {
+  render() {
+    contentArea.innerHTML = `
+      <div class="card-widget">
+        <h4><i class="fas fa-sitemap text-warning me-2"></i>Flows</h4>
+        <p>WhatsApp Flow builder will appear here.</p>
+      </div>
+    `;
+  }
+};
