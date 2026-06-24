06ab902c5e6ae8acf8493c7958597cd1fa04d3ef
allimpliments <allimpliments@gmail.com>
2026-06-24 13:04:39 +0530

    Add Ecommerce rendering functionality

---
+++ js/ecommerce.js
@@ -0,0 +1,10 @@
+const Ecommerce = {
+  render() {
+    contentArea.innerHTML = `
+      <div class="card-widget">
+        <h4><i class="fas fa-store text-dark me-2"></i>E-commerce</h4>
+        <p>WhatsApp Orders, Shopify, WooCommerce integration will appear here.</p>
+      </div>
+    `;
+  }
+};
