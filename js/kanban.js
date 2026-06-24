b916917ca9fe6b5014650cca289edd194ac443c2
allimpliments <allimpliments@gmail.com>
2026-06-24 13:06:32 +0530

    Create kanban.js

---
+++ js/kanban.js
@@ -0,0 +1,10 @@
+const Kanban = {
+  render() {
+    contentArea.innerHTML = `
+      <div class="card-widget">
+        <h4><i class="fas fa-tasks text-info me-2"></i>Kanban Board</h4>
+        <p>Task pipeline (To-Do, In Progress, Done) will appear here.</p>
+      </div>
+    `;
+  }
+};
