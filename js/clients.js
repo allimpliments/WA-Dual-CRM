c12f7bbdcbdc38e206e766e52c7cbe73a9e8b7cd
allimpliments <allimpliments@gmail.com>
2026-06-24 13:06:06 +0530

    Add Clients object with render method

---
+++ js/clients.js
@@ -0,0 +1,10 @@
+const Clients = {
+  render() {
+    contentArea.innerHTML = `
+      <div class="card-widget">
+        <h4><i class="fas fa-building text-secondary me-2"></i>Clients</h4>
+        <p>Client management will appear here.</p>
+      </div>
+    `;
+  }
+};
