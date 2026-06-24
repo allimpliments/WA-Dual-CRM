d7d3d988e8aed3be6e92cfec3dd7625ad20ea357
allimpliments <allimpliments@gmail.com>
2026-06-24 13:05:30 +0530

    Add Agents module for team member management

---
+++ js/agents.js
@@ -0,0 +1,10 @@
+const Agents = {
+  render() {
+    contentArea.innerHTML = `
+      <div class="card-widget">
+        <h4><i class="fas fa-user-tie text-primary me-2"></i>Agents</h4>
+        <p>Team member management will appear here.</p>
+      </div>
+    `;
+  }
+};
