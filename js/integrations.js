1f036115d2eac2ecda039aed2df929892a57bc66
allimpliments <allimpliments@gmail.com>
2026-06-24 13:05:15 +0530

    Add Integrations module with render method

---
+++ js/integrations.js
@@ -0,0 +1,10 @@
+const Integrations = {
+  render() {
+    contentArea.innerHTML = `
+      <div class="card-widget">
+        <h4><i class="fas fa-plug text-success me-2"></i>Integrations</h4>
+        <p>API access, webhook, Google Sheets integration will appear here.</p>
+      </div>
+    `;
+  }
+};
