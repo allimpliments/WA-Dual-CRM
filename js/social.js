b083aca3c54c8c7d01c4281bb13851fc21bd0d00
allimpliments <allimpliments@gmail.com>
2026-06-24 13:06:53 +0530

    Add Social widget rendering functionality

---
+++ js/social.js
@@ -0,0 +1,10 @@
+const Social = {
+  render() {
+    contentArea.innerHTML = `
+      <div class="card-widget">
+        <h4><i class="fas fa-share-alt text-primary me-2"></i>Social Posting</h4>
+        <p>Facebook and Instagram post scheduling will appear here.</p>
+      </div>
+    `;
+  }
+};
