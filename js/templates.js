269afc926461552cb30934275d87119693e39256
allimpliments <allimpliments@gmail.com>
2026-06-24 13:03:48 +0530

    Add Templates object with render method

---
+++ js/templates.js
@@ -0,0 +1,10 @@
+const Templates = {
+  render() {
+    contentArea.innerHTML = `
+      <div class="card-widget">
+        <h4><i class="fas fa-layer-group text-info me-2"></i>Templates</h4>
+        <p>WhatsApp message templates will appear here.</p>
+      </div>
+    `;
+  }
+};
