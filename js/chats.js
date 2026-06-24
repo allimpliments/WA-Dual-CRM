8ee80504b6c299b7ffdb16141c28f9bac6090dc1
allimpliments <allimpliments@gmail.com>
2026-06-24 13:02:54 +0530

    Add Chats module with render method

---
+++ js/chats.js
@@ -0,0 +1,10 @@
+const Chats = {
+  render() {
+    contentArea.innerHTML = `
+      <div class="card-widget">
+        <h4><i class="fas fa-comments text-success me-2"></i>All Chats</h4>
+        <p>WhatsApp, Facebook, Instagram unified inbox will appear here.</p>
+      </div>
+    `;
+  }
+};
