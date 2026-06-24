39fbfcf38c93e70a742a0c4006acaeea82d2c129
allimpliments <allimpliments@gmail.com>
2026-06-24 13:02:32 +0530

    Add Setup object with render method for UI

---
+++ js/setup.js
@@ -0,0 +1,10 @@
+const Setup = {
+  render() {
+    contentArea.innerHTML = `
+      <div class="card-widget">
+        <h4><i class="fas fa-cog text-primary me-2"></i>Setup</h4>
+        <p>WhatsApp Cloud API, Facebook, Instagram setup will appear here.</p>
+      </div>
+    `;
+  }
+};
